6d34927469cc586a5320b8d37aedc317bf4d90df
Lucas Aguirre <aguirrelucas2017@outlook.es>
2022-03-04 00:15:43 -0300

    Gym Rutines Website

---
+++ RutinasGim (html, css y js)/main.js
@@ -0,0 +1,23 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+/* INDEX */ 
+ScrollReveal().reveal('.showcase', { delay: 500 });
+ScrollReveal().reveal('.training-cards', { delay: 500 });
+ScrollReveal().reveal('.showcase2', { delay: 500 });
+ScrollReveal().reveal('.banner', { delay: 500 });
+ScrollReveal().reveal('.reference', { delay: 500 });
+
+/* ALIMENTACION */
+ScrollReveal().reveal('.titulo', { delay: 500 });
+ScrollReveal().reveal('.coments', { delay: 500 });
+
+/* EJERCICIOS */
+ScrollReveal().reveal('.ejer', { delay: 500 });
+
+
+
+
+
+
